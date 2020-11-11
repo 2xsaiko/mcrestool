ed549815627f011c78b276605f99c29bdcbf3e4c
2xsaiko <git@dblsaiko.net>
2020-11-11 11:12:11 +0100

    Remove debug code

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -16,11 +16,19 @@ enum FileType {
 fn get_file_type<P: AsRef<Path>>(ds: &DataSource, path: P) -> Option<FileType> {
     // shitty detection for now
     let path = path.as_ref();
-    if ds.is_file(path) && path.extension().map_or(false, |s| s == OsStr::new("json")) && path.parent().and_then(|p| get_file_type(ds, p)) == Some(FileType::Language) {
+    if ds.is_file(path) && has_extension(path, "json") && path.parent().and_then(|p| get_file_type(ds, p)) == Some(FileType::Language) {
         Some(FileType::LanguagePart)
-    } else if ds.is_dir(path) && path.file_name().map_or(false, |s| s == OsStr::new("lang")) {
+    } else if ds.is_dir(path) && has_file_name(path, "lang") {
         Some(FileType::Language)
     } else {
         None
     }
+}
+
+fn has_extension<P: AsRef<Path>, S: AsRef<OsStr>>(path: P, ext: S) -> bool {
+    path.as_ref().extension().map_or(false, |s| s == ext.as_ref())
+}
+
+fn has_file_name<P: AsRef<Path>, S: AsRef<OsStr>>(path: P, name: S) -> bool {
+    path.as_ref().file_name().map_or(false, |s| s == name.as_ref())
 }
